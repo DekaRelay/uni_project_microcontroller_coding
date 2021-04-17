c199a91ce1aded89b0d619d82175e7f7a2c61c82
Henry Geonwoo Kim <72729291+dmrdpr@users.noreply.github.com>
2021-04-17 11:11:31 +1000

    Add files via upload

--- Lab2/Sources/main.c
+++ Lab2/Sources/main.c
@@ -2,141 +2,34 @@
 #include "derivative.h"      /* derivative-specific definitions */
 #include <math.h>
 #include "timers.h"
+#include "t_data.h"
 
-int count;                        // global variable for timer counts
 
 void main(void) {
   /* put your own code here */
  
   //  Variable initilisations
-  volatile int a, b, c;           // integer calculations
-  volatile long d, e, f;
-  
-  unsigned start_time, end_time;  // Start and end time
-  unsigned start_count, end_count; 
-  //unsigned time[4][7];            // Result times
-  
-  typedef struct {                // Struct for time data
-    int add;
-    int mult;
-    int div;
-    int sqrt;
-    int sin;
-    int cos;  
-  }time;
-  
-  
-  // Initialise each time structs  (integer, long, 32 & 64 bit floats)
-  time t_int;
-  time t_long;
-  time t_32f;
-  time t_64f;
-  
-  unsigned reps;
 
+  
+  
+  int time[4][6];            // Result times
+  int* time_loc;
+  int i;
+  
+  
   // Initiate timer
   Init_TC7();
+  
+  time_loc = generate_time(0);
 
-
-  
-  // (1) Addition
-  b = 8;
-  c = 5;
-  
-  start_count = count;
-  start_time = TCNT;
-  
-  a = b + c;
-  
-  end_time = TCNT; 
-  end_count = count;
-  
-  start_time = start_time + 0xFF*start_count;
-  end_time = end_time + 0xFF*end_count;
-  
-  
-  t_int.add = end_time - start_time;
-  
-    
-  // (2) Multiply
-  start_count = count;
-  start_time = TCNT;
-  
-  a = b * c;
-  
-  end_time = TCNT; 
-  end_count = count;
-  
-  start_time = start_time + 0xFF*start_count;
-  end_time = end_time + 0xFF*end_count;
-  
-  
-  t_int.mult = end_time - start_time;
-  
-  
-  // (3) Division
-  start_count = count;
-  start_time = TCNT;
-  
-  a = b / c;
-  
-  end_time = TCNT; 
-  end_count = count;
-  
-  start_time = start_time + 0xFF*start_count;
-  end_time = end_time + 0xFF*end_count;
-  
-  
-  t_int.div = end_time - start_time;
-  
-  
-  // (4) Square root
-  start_count = count;
-  start_time = TCNT;
-  
-  a = sqrt(b);
-  
-  end_time = TCNT; 
-  end_count = count;
-  
-  start_time = start_time + 0xFF*start_count;
-  end_time = end_time + 0xFF*end_count;
-  
-  
-  t_int.sqrt = end_time - start_time;
-  
-  
-  // (5) Sine
-  start_count = count;
-  start_time = TCNT;
-  
-  a = sin(b);
-  
-  end_time = TCNT; 
-  end_count = count;
-  
-  start_time = start_time + 0xFF*start_count;
-  end_time = end_time + 0xFF*end_count;
-  
-  
-  t_int.sin = end_time - start_time;
-  
-   
-  // (6) Cosine
-  start_count = count;
-  start_time = TCNT;
-  
-  a = cos(b);
-  
-  end_time = TCNT; 
-  end_count = count;
-  
-  start_time = start_time + 0xFF*start_count;
-  end_time = end_time + 0xFF*end_count;
-  
-  
-  t_int.cos = end_time - start_time;  
-  
+  for ( i = 0; i < 6; i++ ) {
+      time[0][i] = *(time_loc + i);
+      time[1][i] = *(time_loc + i + 6);
+      time[2][i] = *(time_loc + i + 12);
+      time[3][i] = *(time_loc + i + 18);     
+  }
+ 
+ 
   
 	EnableInterrupts;
 
---
+++ Lab2/Sources/t_data.h
@@ -0,0 +1,10 @@
+#ifndef TIME_GENERATE_H
+#define TIME_GENERATE_H
+
+
+int* generate_time(int err);
+
+  
+
+
+#endif
